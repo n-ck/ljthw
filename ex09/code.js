232568e9ebffceef4eb8942d6bf1967db6c1cac0
Nick Kuizenga <nickkuis@hotmail.com>
2019-02-21 22:34:06 -0500

    finished exercise 9

---
+++ ex09/code.js
@@ -0,0 +1,8 @@
+let name = process.argv[2];
+let age = process.argv[3];
+let eyes = process.argv[4];
+
+console.log(`Your name is ${name}`);
+console.log(`Your age is ${age}`);
+console.log(`Your eyes are ${eyes}`);
+
