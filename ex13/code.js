bcba8e03884727cbb6b6ec53ccaa4b358e9d255c
Nick Kuizenga <nickkuizenga@gmail.com>
2019-02-27 02:16:07 -0800

    Started ex13

---
+++ ex13/code.js
@@ -0,0 +1,7 @@
+const geometry = require('./geometry');
+
+let area51 = geometry.area(2.8);
+let circ2 = geometry.circumference(6);
+
+console.log(`Area: ${area51}, Circumference: ${circ2}`);
+
---
+++ ex13/geometry.js
@@ -0,0 +1,8 @@
+const area = (r) => Math.PI * r * 2;
+
+const circumference = (r) => 2 * Math.PI * r;
+
+module.exports = {
+	area: area,
+	circumference: circumference
+}
