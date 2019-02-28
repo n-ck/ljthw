0bf2290bff01620e58b38cd8656bc4b1baf4fe8c
Nick Kuizenga <nickkuizenga@gmail.com>
2019-02-28 03:18:44 -0800

    Finished exercise 13

--- ex13/code.js
+++ ex13/code.js
@@ -2,6 +2,7 @@ const geometry = require('./geometry');
 
 let area51 = geometry.area(2.8);
 let circ2 = geometry.circumference(6);
+let dia = geometry.diameter(3.2);
 
-console.log(`Area: ${area51}, Circumference: ${circ2}`);
+console.log(`Area: ${area51}, Circumference: ${circ2}, Diameter: ${dia}`);
 
--- ex13/geometry.js
+++ ex13/geometry.js
@@ -1,8 +1,11 @@
-const area = (r) => Math.PI * r * 2;
+const area = (r) => Math.PI * Math.pow(r, 2);
 
 const circumference = (r) => 2 * Math.PI * r;
 
+const diameter = (r) => 2 * r;
+
 module.exports = {
 	area: area,
-	circumference: circumference
+	circumference: circumference,
+	diameter: diameter
 }
