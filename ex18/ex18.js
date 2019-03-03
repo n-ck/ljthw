dbbd5419223720ff3eccb2af64e5416320fdcf74
Nick Kuizenga <nickkuizenga@gmail.com>
2019-03-03 03:26:44 -0800

    Finished exercise 18

---
+++ ex18/ex18.js
@@ -0,0 +1,18 @@
+exports.fruit = [
+	['Apples', 12, 'AAA'], ['Oranges', 1, 'B'],
+	['Pears', 3, 'A'], ['Grapes', 14, 'UR']];
+
+exports.cars = [
+	['Cadillac', ['Black', 'Big', 34500]],
+	['Corvette', ['Red', 'Little', 1000000]],
+	['Ford', ['Blue', 'Medium', 1234]],
+	['BMW', ['White', 'Baby', 7890]]
+];
+
+exports.languages = [
+	['Python', ['Slow', ['Terrible', 'Mush']]],
+	['JavaScript', ['Moderate', ['Alright', 'Bizarre']]],
+	['Perl6', ['Moderate', ['Fun', 'Weird']]],
+	['C', ['Fast', ['Annoying', 'Dangerous']]],
+	['Forth', ['Fast', ['Fun', 'Difficult']]],
+];
