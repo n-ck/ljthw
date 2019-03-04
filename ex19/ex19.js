6f98509572d1b281a200137b5fbee25fa45d5bb4
Nick Kuizenga <nickkuizenga@gmail.com>
2019-03-04 04:21:22 -0800

    Started exercise 19

---
+++ ex19/ex19.js
@@ -0,0 +1,15 @@
+exports.fruit = [
+	{kind: 'Apples', count: 12, rating: 'AAA'},
+	{kind: 'Oranges', count: 1, rating: 'B'},
+	{kind: 'Pears', count: 2, rating: 'A'},
+	{kind: 'Grapes', count: 14, rating: 'UR'}
+];
+
+/*
+exports.cars = [
+	{type: 'Cadillac', color: 'Black', size: 'Big', miles: 34500},
+	{type: 'Corvette', color: 'Red', size: 'Little', miles: 1000000},
+    {type: 'Ford', color: 'Blue', size: 'Medium', miles: 1234},
+    {type: 'BMW', color: 'White', size: 'Baby', miles: 7890}
+];
+*/
