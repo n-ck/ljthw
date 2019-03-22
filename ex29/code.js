86fd261263fdcefbf689811124fd1c4a1e070f24
Nick Kuizenga <nickkuis@hotmail.com>
2019-03-21 22:13:25 -0400

    Started ex29

---
+++ ex29/code.js
@@ -0,0 +1,6 @@
+let pets = [
+    {name: 'Yeller', type: 'Dog', age: 12},
+    {name: 'Akumano', type: 'Japanese Bobtail Cat', age: 2},
+    {name: 'Meaw Peesard', type: 'Siamese Cat', age: 100},
+    {name: 'James', type: 'Gecko', age: 2},
+]
