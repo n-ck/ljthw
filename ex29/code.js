d0c39075e7641dfda2226646a2507bca153426c1
Nick Kuizenga <nickkuis@hotmail.com>
2019-03-22 21:09:02 -0400

    Continued exercise 29

--- ex29/code.js
+++ ex29/code.js
@@ -4,3 +4,45 @@ let pets = [
     {name: 'Meaw Peesard', type: 'Siamese Cat', age: 100},
     {name: 'James', type: 'Gecko', age: 2},
 ]
+
+const young_pets = (pet) => {
+    return pet.age <= 10;
+}
+
+const age_pets = (pet) => {
+    return [pet.name, pet.age + 10];
+}
+
+const name_age = (pet) => {
+    return `${pet[0]} is ${pet[1]}`
+}
+
+const debug = (msg) => {
+    console.log(msg);
+    return msg;
+}
+
+const tee = (result, data, cb) => {
+    var side = (input) => {
+        cb(input, result, data);
+        return input;
+    }
+
+    return side;
+}
+
+let owned_pets = [];
+const add_owner_tee = tee(owned_pets, 'Zed', (i, r, d) => {
+    r.push({pet: i, owner: d});
+});
+
+let age_young_animals = pets.filter(young_pets)
+    .map(add_owner_tee)
+    .map(age_pets)
+    .map(name_age)
+
+console.log("-- Pets with Owners:");
+owned_pets.forEach(debug);
+
+console.log("-- Young Animals:");
+age_young_animals.forEach(debug);
