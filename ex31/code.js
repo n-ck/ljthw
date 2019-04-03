8937871ca3e1552773bb1dcd1809072ab0b7c0f6
Nick Kuizenga <nickkuis@hotmail.com>
2019-04-02 22:17:42 -0400

    Finished ex31

--- ex31/code.js
+++ ex31/code.js
@@ -4,18 +4,33 @@
 // Get a first command line arg to work in js file
 // Expirement with more difficult commandl ine args
 
+// Documentation: https://github.com/75lb/command-line-args
+
 
 // import command-line-args
 const commandLineArgs = require('command-line-args');
 
 // make option object
 const myfirstargs = [
-    { name: 'name', alias: 'n', type: String }
+    { name: 'name', alias: 'n', type: String },
+    { name: 'age', alias: 'a', type: Number },
+    { name: 'gender', alias: 'g', type: Boolean }
+
 ]
 
 // set the options
-const printname = commandLineArgs(myfirstargs)
+const person = commandLineArgs(myfirstargs)
 
 // node code.js --name=Nick
 // or: node.code.js -n Nick
-console.log(`My first command line arg: ${printname['name']}`);
+
+console.log(`My name is: ${person['name']}`);
+console.log(`I am ${person['age']} old`);
+
+if (person['gender'] === true) {
+    let gender = "male";
+    console.log(`I am a ${gender}`);
+} else {
+    let gender = "female";
+    console.log(`I am a ${gender}`);
+}
